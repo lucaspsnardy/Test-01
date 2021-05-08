ed1ba85e3de7fa48549efcba359bb693c87f3ee5
lucaspsnardy <79163022+lucaspsnardy@users.noreply.github.com>
2021-05-08 12:50:38 -0400

    Add files via upload

---
+++ age.py
@@ -0,0 +1,8 @@
+N = input("Enter your name: ")
+print ("Hello", N )
+Day = int(input("Enter your day of birth DD: "))
+Month = int(input("Enter your month of birth MM: "))
+Year = int(input("Enter your year of birth YYYY: "))
+print("Your birthday is", Day, Month, Year)
+one_hundred = (100 + Year)
+print(N, "you will be 100 in" ,Month, Day, one_hundred)
